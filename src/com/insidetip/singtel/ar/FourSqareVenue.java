505e42f9716ce631019e86520eb4ace198d65675
Peter Paul Alicaway <petzalicaway@gmail.com>
2010-07-11 09:29:19 +0800

    - Partial AR Package

---
+++ src/com/insidetip/singtel/ar/FourSqareVenue.java
@@ -0,0 +1,10 @@
+package com.insidetip.singtel.ar;
+
+import android.content.Context;
+
+public class FourSqareVenue extends ARSphericalView {
+
+	public FourSqareVenue(Context context) {
+		super(context);
+	}
+}
